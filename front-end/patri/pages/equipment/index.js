1055c54525039a459a653ba7919e387097b3a70f
Vinícius Figueiroa <vinissfigueiroa@gmail.com>
2021-08-06 00:19:01 -0300

    Modal added to equipment page

--- front-end/patri/pages/equipment/index.js
+++ front-end/patri/pages/equipment/index.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button } from '../../src/components/common/Button';
 import { Input } from '../../src/components/common/Input';
 import { PageWrapper } from '../../src/components/PageWrapper';
 import { Table } from '../../src/components/Table';
 import { AiOutlineSearch } from 'react-icons/ai';
+import Modal from '../../src/components/common/Modal';
+import { FormWrapper } from '../../src/components/FormWrapper';
 
 const roomsData = [
   {
@@ -37,6 +39,35 @@ const roomsColumns = [
 ];
 
 export default function Equipment() {
+  const [showModal, setShowModal] = useState(false);
+  const [equipmentDeveloper, setEquipmentDeveloper] = useState('');
+  const [equipmentType, setEquipmentType] = useState('');
+  const [equipmentSerialNumber, setEquipmentSerialNumber] = useState('');
+  const [equipmentDescription, setEquipmentDescription] = useState('');
+  const [equipmentPatrimonyNumber, setEquipmentPatrimonyNumber] = useState('');
+  const [equipmentStatus, setEquipmentStatus] = useState('');
+
+  function cleanInputs() {
+    setEquipmentDeveloper('');
+    setEquipmentType('');
+    setEquipmentSerialNumber('');
+    setEquipmentDescription('');
+    setEquipmentPatrimonyNumber('');
+    setEquipmentStatus('');
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(equipmentDeveloper);
+    console.log(equipmentType);
+    console.log(equipmentSerialNumber);
+    console.log(equipmentDescription);
+    console.log(equipmentPatrimonyNumber);
+    console.log(equipmentStatus);
+    setShowModal(false);
+    cleanInputs();
+  }
+
   return (
     <PageWrapper>
       <h1>Equipamentos</h1>
@@ -47,8 +78,63 @@ export default function Equipment() {
         />
         <div className="tableButtons">
           <Button colorType="default">Filtrar</Button>
-          <Button colorType="done">+ Novo equipamento</Button>
+          <Button colorType="done" onClick={() => setShowModal(true)}>
+            + Novo equipamento
+          </Button>
         </div>
+        <Modal onClose={() => setShowModal(false)} show={showModal}>
+          <FormWrapper>
+            <h1 className="modalTitle">+ Nova sala</h1>
+            <form onSubmit={(e) => handleSubmit(e)}>
+              <div className="formInputs">
+                <Input
+                  placeholder="Marca"
+                  value={equipmentDeveloper}
+                  onChange={(e) => setEquipmentDeveloper(e.target.value)}
+                />
+                <Input
+                  placeholder="Tipo"
+                  value={equipmentType}
+                  onChange={(e) => setEquipmentType(e.target.value)}
+                />
+                <Input
+                  placeholder="Nº de série"
+                  value={equipmentSerialNumber}
+                  onChange={(e) => setEquipmentSerialNumber(e.target.value)}
+                />
+                <Input
+                  placeholder="Descrição"
+                  value={equipmentDescription}
+                  onChange={(e) => setEquipmentDescription(e.target.value)}
+                />
+                <Input
+                  placeholder="Nº de patrimônio"
+                  value={equipmentPatrimonyNumber}
+                  onChange={(e) => setEquipmentPatrimonyNumber(e.target.value)}
+                />
+                <Input
+                  placeholder="Status"
+                  value={equipmentStatus}
+                  onChange={(e) => setEquipmentStatus(e.target.value)}
+                />
+              </div>
+              <div className="links modal">
+                <Button
+                  colorType="cancel"
+                  onClick={() => {
+                    setShowModal(false);
+                    cleanInputs();
+                  }}
+                >
+                  Cancelar
+                </Button>
+                <Button colorType="done" type="submit">
+                  Entrar
+                </Button>
+              </div>
+            </form>
+          </FormWrapper>
+        </Modal>
       </div>
       <Table columns={roomsColumns} data={roomsData} role="1" />
     </PageWrapper>
